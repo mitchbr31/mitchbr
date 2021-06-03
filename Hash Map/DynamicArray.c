4accf7f4cad1c0e78bb23f4fce1ceea283dfbe00
mitchbr <brownmit@oregonstate.edu>
2021-06-03 14:27:23 -0700

    Initializes Empty array

--- Hash Map/DynamicArray.c
+++ Hash Map/DynamicArray.c
@@ -3,19 +3,32 @@
 
 struct array
 {
-    int *array;
-    size_t mem_size;
-    size_t length;
+    int *array; // The array
+    size_t max_length; // Maximum length of the currently allocated static array
+    size_t length; // Current length of the array
 };
 
-struct array *createArray (size_t length)
+struct array *createArray (size_t max_length, struct array *oldArr)
 {
     /* Initialize an empty array */
-    struct array *newArr = malloc(length*sizeof(int)); // ONLY WORKS ON INTS
+    struct array *newArr = malloc(sizeof(struct array));
+    newArr->array = malloc(max_length*sizeof(int)); // ONLY WORKS ON INTS
+    newArr->max_length = max_length;
+    newArr->length = 0;
+
+    if (oldArr != NULL)
+    {
+        for (int i = 0; i == oldArr->length; i++)
+        {
+            newArr->array[i] = oldArr->array[i];
+            newArr->length++;
+        }
+    }
+    
     return newArr;
 }
 
-struct array *appendArray (int *array, size_t mem_size, size_t length)
+struct array *appendArray ()
 {
     /* Add elements to the array, reallocating as necessary */
     return 0;
@@ -23,6 +36,8 @@ struct array *appendArray (int *array, size_t mem_size, size_t length)
 
 int
 main () {
-    struct array *arr1 = createArray(4);
+    struct array *arr1 = createArray(4, NULL);
+    arr1->array[0] = 1;
+    printf("%zu\n", arr1->length);
     return 0;
 }
