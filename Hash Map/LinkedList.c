32f58dd768a60b6b12b38769558b3b8f9df5099e
mitchbr <brownmit@oregonstate.edu>
2021-06-03 17:37:57 -0700

    LinkedList initializes new list

--- Hash Map/LinkedList.c
+++ Hash Map/LinkedList.c
@@ -1,6 +1,36 @@
-struct linkedList
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct listNode
 {
     char *key;
     int value;
-    struct linkedList *next;
+    struct listNode *next;
 };
+
+struct linkedList
+{
+    size_t length;
+    struct listNode *head;
+    struct listNode *tail;
+};
+
+struct linkedList *initList ()
+{
+    struct linkedList *newList = malloc(sizeof(struct linkedList));
+    newList->head = malloc(sizeof(struct listNode));
+    newList->tail = malloc(sizeof(struct listNode));
+
+    newList->head->next = newList->tail;
+
+    return newList;
+};
+
+
+int main ()
+{
+    struct listNode *testNode = malloc(sizeof(struct listNode));
+
+    struct linkedList *dict = initList();
+}
