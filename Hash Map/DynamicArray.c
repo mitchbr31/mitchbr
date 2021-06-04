7134171cbf82ec6970405b0a1573d7663c456a80
mitchbr <brownmit@oregonstate.edu>
2021-06-04 11:02:17 -0700

    Dynamic array reallocates

--- Hash Map/DynamicArray.c
+++ Hash Map/DynamicArray.c
@@ -11,7 +11,7 @@ struct dynamicArray
 void createArray (size_t max_length, struct dynamicArray *newArr)
 {
     /* Initialize an empty array */
-    newArr->array = malloc(max_length * sizeof(int));
+    newArr->array = (int*) malloc(max_length * sizeof(int));
     newArr->max_length = max_length;
     newArr->length = 0;
 }
@@ -19,24 +19,37 @@ void createArray (size_t max_length, struct dynamicArray *newArr)
 void appendArray (struct dynamicArray *arr, int value)
 {
     /* Add elements to the array, reallocating as necessary */
+    if (arr->max_length == arr->length)
+    {
+        printf("reallocating\n");
+        struct dynamicArray newArr;
+        createArray(arr->max_length * 2, &newArr);
+        newArr.length = arr->length;
+        for (int i = 0; i < arr->length; i++)
+            newArr.array[i] = arr->array[i];
+        *arr = newArr;
+    }
+
     arr->array[arr->length] = value;
     arr->length++;
 }
 
 int
 main () {
-    struct dynamicArray *arr1 = malloc(sizeof(struct dynamicArray));
-    createArray(4, arr1);
-    appendArray(arr1, 1);
-    appendArray(arr1, 2);
-    appendArray(arr1, 3);
-    appendArray(arr1, 4);
-    appendArray(arr1, 5);
-    for (int i = 0; i < arr1->length; i++)
+    struct dynamicArray arr1;
+    createArray(4, &arr1);
+    appendArray(&arr1, 1);
+    appendArray(&arr1, 2);
+    appendArray(&arr1, 3);
+    appendArray(&arr1, 4);
+    appendArray(&arr1, 5);
+    appendArray(&arr1, 6);
+
+    for (int i = 0; i < arr1.length; i++)
     {
-        printf("%d\n", arr1->array[i]);
+        printf("%d, %d\n", i, arr1.array[i]);
     }
-    printf("%lu\n", sizeof(arr1->array));
+    printf("%zu\n", arr1.max_length);
 
     return 0;
 }
