06da2e74bc43445b6c951e1733def886383c145f
mitchbr <brownmit@oregonstate.edu>
2021-06-04 17:38:04 -0700

    Added hashMap struct

--- Hash Map/HashMap.h
+++ Hash Map/HashMap.h
@@ -1,8 +1,18 @@
+#include "DynamicArray.h"
+#include "LinkedList.h"
+
 #include <stdlib.h>
 
 #ifndef HASH_MAP_H
 #define HASH_MAP_H
 
+struct hashMap
+{
+    struct linkedList   *buckets;
+    size_t              capacity;
+    size_t              size;
+};
 
+int hashFunction ( char *key);
 
 #endif
